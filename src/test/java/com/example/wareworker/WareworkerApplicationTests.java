221e7e6ca85005c5d5976835c1dfd03fa76f0fc6
TheScarface43 <barti123.96@gmail.com>
2023-10-09 19:28:06 +0200

    first commit

---
+++ src/test/java/com/example/wareworker/WareworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wareworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
