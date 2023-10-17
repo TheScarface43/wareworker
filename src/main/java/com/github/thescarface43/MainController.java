6932e3e8ef4fef21e728ab7bc4f228079aa5cbc1
TheScarface43 <barti123.96@gmail.com>
2023-10-17 17:49:50 +0200

    change package name

--- src/main/java/com/github/thescarface43/MainController.java
+++ src/main/java/com/github/thescarface43/MainController.java
@@ -1,4 +1,4 @@
-package com.example.wareworker;
+package com.github.thescarface43;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/github/thescarface43/Start.java
+++ src/main/java/com/github/thescarface43/Start.java
@@ -1,4 +1,4 @@
-package com.example.wareworker;
+package com.github.thescarface43;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
