7fb53312d328a9f086058d4017b06ebda55d8bd7
TheScarface43 <barti123.96@gmail.com>
2023-10-25 19:45:00 +0200

    fix errors in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS warehouse_user (
-    user_id SERIAL PRIMARY KEY,
-    login VARCHAR(64) UNIQUE NOT NULL,
-    password CHAR(85) NOT NULL,
-    enabled BIT NOT NULL DEFAULT 1,
+    id SERIAL PRIMARY KEY,
+    user_login VARCHAR(64) UNIQUE NOT NULL,
+    user_password CHAR(85) NOT NULL,
+    is_enabled BIT NOT NULL DEFAULT '1',
     first_name VARCHAR(32) NOT NULL,
     last_name VARCHAR(32) NOT NULL,
     email VARCHAR(64),
@@ -10,22 +10,22 @@ CREATE TABLE IF NOT EXISTS warehouse_user (
 );
 
 CREATE TABLE IF NOT EXISTS warehouse_type (
-    warehouse_type_id SERIAL PRIMARY KEY,
-    name VARCHAR(64) UNIQUE NOT NULL,
+    id SERIAL PRIMARY KEY,
+    type_name VARCHAR(64) UNIQUE NOT NULL,
     description VARCHAR(256)
 );
 
 CREATE TABLE IF NOT EXISTS warehouse (
-    warehouse_id SERIAL PRIMARY KEY,
-    name VARCHAR(64) UNIQUE NOT NULL,
-    location VARCHAR(128),
+    id SERIAL PRIMARY KEY,
+    warehouse_name VARCHAR(64) UNIQUE NOT NULL,
+    warehouse_location VARCHAR(128),
     description VARCHAR(256),
     warehouse_type_id INT NOT NULL,
-    FOREIGN KEY (warehouse_type_id) REFERENCES warehouse_type (warehouse_type_id)
+    FOREIGN KEY (warehouse_type_id) REFERENCES warehouse_type (id)
 );
 
 CREATE TABLE IF NOT EXISTS permission (
-    permission_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     permission INT NOT NULL,
     -- 000000 - read only, 
     -- 000001 - add ware amounts to locations,
@@ -36,40 +36,40 @@ CREATE TABLE IF NOT EXISTS permission (
     -- 100000 - global admin (add, remove and change all warehouses and all users)
     -- mix and match for a desired set of permissions, for example 000011 to move wares to and from locations, or 011111 for total control over one warehouse
     user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES warehouse_user (user_id),
+    FOREIGN KEY (user_id) REFERENCES warehouse_user (id),
     warehouse_id INT,
-    FOREIGN KEY (warehouse_id) REFERENCES warehouse (warehouse_id),
+    FOREIGN KEY (warehouse_id) REFERENCES warehouse (id),
     CHECK (permission >= 32 or warehouse_id is not null)
 );
 
 CREATE TABLE IF NOT EXISTS location_type (
-    location_type_id SERIAL PRIMARY KEY,
-    name VARCHAR(64)
+    id SERIAL PRIMARY KEY,
+    type_name VARCHAR(64)
 );
 
 CREATE TABLE IF NOT EXISTS location (
-    location_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     rack VARCHAR(16) NOT NULL,
     spot INT NOT NULL,
     level INT NOT NULL,
     warehouse_id INT NOT NULL,
-    FOREIGN KEY (warehouse_id) REFERENCES warehouse (warehouse_id),
+    FOREIGN KEY (warehouse_id) REFERENCES warehouse (id),
     location_type_id INT NOT NULL,
-    FOREIGN KEY (location_type_id) REFERENCES location_type (location_type_id),
+    FOREIGN KEY (location_type_id) REFERENCES location_type (id),
     CONSTRAINT unique_location UNIQUE (rack, spot, level, warehouse_id)
 );
 
 CREATE TABLE IF NOT EXISTS ware_category (
-    ware_category_id SERIAL PRIMARY KEY,
-    name VARCHAR(64) UNIQUE NOT NULL,
+    id SERIAL PRIMARY KEY,
+    category_name VARCHAR(64) UNIQUE NOT NULL,
     description VARCHAR(256)
 );
 
 CREATE TABLE IF NOT EXISTS ware (
-    ware_id SERIAL PRIMARY KEY,
-    name VARCHAR(64) UNIQUE NOT NULL,
+    id SERIAL PRIMARY KEY,
+    ware_name VARCHAR(64) UNIQUE NOT NULL,
     unit VARCHAR(10) NOT NULL,
-    permit_decimal BIT NOT NULL DEFAULT 1,
+    permit_decimal BIT NOT NULL DEFAULT '1',
     ean VARCHAR(13) UNIQUE,
     price DECIMAL(10,2),
     vat INT,
@@ -77,22 +77,22 @@ CREATE TABLE IF NOT EXISTS ware (
     max_per_pallet INT,
     description VARCHAR(256),
     ware_category_id INT NOT NULL,
-    FOREIGN KEY (ware_category_id) REFERENCES ware_category (ware_category_id)
+    FOREIGN KEY (ware_category_id) REFERENCES ware_category (id)
 );
 
 CREATE TABLE IF NOT EXISTS operation (
-    operation_id SERIAL PRIMARY KEY,
-    type INT NOT NULL,
+    id SERIAL PRIMARY KEY,
+    operation_type INT NOT NULL,
     amount DECIMAL(10,5) NOT NULL,
-    operation_timestamp TIMESTAMP NOT NULL,
+    operation_time TIMESTAMP NOT NULL,
     expiration_date date,
     ware_id INT NOT NULL,
-    FOREIGN KEY (ware_id) REFERENCES ware (ware_id),
+    FOREIGN KEY (ware_id) REFERENCES ware (id),
     location_id INT NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES location (location_id),
+    FOREIGN KEY (location_id) REFERENCES location (id),
     user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES warehouse_user (user_id)
+    FOREIGN KEY (user_id) REFERENCES warehouse_user (id)
 );
 
-INSERT INTO warehouse_user (login, password, first_name, last_name) VALUES ('global_admin', 'change_me', 'Global', 'Admin');
+INSERT INTO warehouse_user (user_login, user_password, first_name, last_name) VALUES ('global_admin', 'change_me', 'Global', 'Admin');
 INSERT INTO permission (permission, user_id) VALUES (63, 1);
